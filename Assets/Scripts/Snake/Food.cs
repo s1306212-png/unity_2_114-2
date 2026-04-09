be0720f55d085571cce8ec54b9497f74f7ab401a
s1315119-a11y <s1315119@taivs.tp.edu.tw>
2026-04-09 16:20:28 +0800

    4/9 update

---
+++ Assets/Scripts/Snake/Food.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public BoxCollider2D gridArea;
+
+    private void Start()
+    {
+        RandomizePosition();
+    }
+
+    // Update is called once per frame
+
+    private void RandomizePosition()
+    {
+        Bounds bounds = this.gridArea.bounds;
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        this.transform.position = new Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            RandomizePosition();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Snake/Snake.cs
@@ -0,0 +1,76 @@
+using System.Collections.Generic;
+using System.Xml;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 _direction = Vector2.right;
+
+    private List<Transform> _segments;
+
+    public Transform segmentPrefab;
+
+    private void Start()
+    {
+        _segments = new List<Transform>
+        {
+            this.transform
+        };
+    }
+
+    private void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            _direction = Vector2.up;
+        }
+        else if(Input.GetKeyDown(KeyCode.S))
+        {
+            _direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            _direction = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _direction = Vector2.right;
+        }
+    }
+    private void FixedUpdate()
+    {
+        for (int i = _segments.Count - 1; i > 0; i--)
+        {
+            _segments[i].position = _segments[i - 1].position;
+        }
+        this.transform.position = new Vector3(
+            Mathf.Round(this.transform.position.x + _direction.x),
+            Mathf.Round(this.transform.position.y + _direction.y),
+            0.0f
+        );
+    }
+    private void Grow()
+    {
+        Transform segment = Instantiate(this.segmentPrefab);
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add(segment);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Food")
+        {
+            Grow();
+        }else if (other.tag == "Obstacle")
+        {
+            ResetState();
+        }
+
+    }
+    private void ResetState()
+    {
+
+    }
+}
